dd621a46589e889d9a4d2b0690722ca83399da5c
Crazy24699 <110395759+Crazy24699@users.noreply.github.com>
2024-04-14 21:38:43 +0200

    Update

--- Summoners Delight/Assets/Scripts/World scriots/UIFunction.cs
+++ Summoners Delight/Assets/Scripts/World scriots/UIFunction.cs
@@ -33,7 +33,7 @@ public class UIFunction : MonoBehaviour
 
     public IEnumerator FindScriptDelay()
     {
-        yield return new WaitForSeconds(1.0f);
+        yield return new WaitForSeconds(0.10f);
         SpawningScript = FindObjectOfType<MinionSpawning>();
         SpawningScript.SpawnNum = 1;
     }
