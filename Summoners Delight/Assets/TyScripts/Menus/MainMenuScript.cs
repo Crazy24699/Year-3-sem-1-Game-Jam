8c10e8ddde7f1863a8b7e4ef87506761c50cb613
TGFo <102477641+TGFo@users.noreply.github.com>
2024-04-14 20:25:03 +0200

    started options menu

---
+++ Summoners Delight/Assets/TyScripts/Menus/MainMenuScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public GameObject optionsPanel;
+    public GameObject MenuPanel;
+    public void StartButton()
+    {
+
+    }
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+    public void OptionsButton()
+    {
+
+    }
+}
